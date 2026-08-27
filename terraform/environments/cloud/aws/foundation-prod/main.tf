d71259165aca643558e00e8f5482a7f69742a6c8
hushamsadd <hushamsadd@gmail.com>
2026-08-27 21:16:30 +0200

    fix: change default vm_user to ubuntu and add missing safety nets to SSM/SSH wait loops in CI pipelines

--- terraform/environments/cloud/aws/foundation-prod/main.tf
+++ terraform/environments/cloud/aws/foundation-prod/main.tf
@@ -114,6 +114,14 @@ resource "aws_vpc_security_group_egress_rule" "all_ipv4" {
   ip_protocol       = "-1"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+
 #################################
 # Systems Manager
 #################################
