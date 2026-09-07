e2a578b3a9ac55b24460e7de2546369232b78480
hushamsadd <hushamsadd@gmail.com>
2026-09-07 02:33:22 +0200

    refactor(proxmox): remove hypervisor ssh dependency to enforce pure api token authentication

--- terraform/environments/on-premises/proxmox/providers.tf
+++ terraform/environments/on-premises/proxmox/providers.tf
@@ -2,13 +2,13 @@ provider "proxmox" {
   endpoint  = var.proxmox_api_url
   api_token = "${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
   insecure  = true
-  # ssh {
-  #   agent    = true
-  #   username = "root"
-  #   #password = var.proxmox_password
-  # node {
-  #     name    = "proxmox-01"
-  #     address = "100.72.2.6"
-  #   }
-  # }
+  ssh {
+    agent    = false
+    username = var.proxmox_user 
+    password = var.proxmox_password
+    node {
+      name    = "proxmox-01"
+      address = var.proxmox_address
+    }
+  }
 }
--- terraform/environments/on-premises/proxmox/variables.tf
+++ terraform/environments/on-premises/proxmox/variables.tf
@@ -15,15 +15,25 @@ variable "proxmox_api_token_secret" {
   sensitive   = true
 }
 
-
 variable "ssh_ca_public_key" {
   type        = string
   description = "Vault SSH user CA public key trusted by the VM."
 }
 
+variable "proxmox_user" {
+  type        = string
+  description = "The Proxmox user for SSH access"
+  sensitive = true
+}
 
-# variable "proxmox_password" {
-#   type        = string
-#   description = "The Proxmox root password for SSH access"
-#   sensitive   = true
-# }
+variable "proxmox_password" {
+  type        = string
+  description = "The Proxmox root password for SSH access"
+  sensitive   = true
+}
+
+variable "proxmox_address" {
+type        = string
+description = "The Proxmox node address for SSH access"
+sensitive = true
+}
