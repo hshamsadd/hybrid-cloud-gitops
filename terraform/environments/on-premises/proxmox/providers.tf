4a75740d13735402e95657a0da39e28fe891cd88
hushamsadd <hushamsadd@gmail.com>
2026-09-07 02:48:44 +0200

    refactor(proxmox): remove hypervisor ssh dependency to enforce pure api token authentication

--- terraform/environments/on-premises/proxmox/providers.tf
+++ terraform/environments/on-premises/proxmox/providers.tf
@@ -1,14 +1,21 @@
 provider "proxmox" {
-  endpoint  = var.proxmox_api_url
-  api_token = "${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
-  insecure  = true
-  ssh {
-    agent    = false
-    username = var.proxmox_user 
-    password = var.proxmox_password
-    node {
-      name    = "proxmox-01"
-      address = var.proxmox_address
-    }
-  }
+  endpoint = var.proxmox_api_url
+  username = "${var.proxmox_user}@pam" 
+  password = var.proxmox_password  
+  insecure = true
 }
+
+# provider "proxmox" {
+#   endpoint  = var.proxmox_api_url
+#   api_token = "${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
+#   insecure  = true
+#   ssh {
+#     agent    = false
+#     username = var.proxmox_user 
+#     password = var.proxmox_password
+#     node {
+#       name    = "proxmox-01"
+#       address = var.proxmox_address
+#     }
+#   }
+# }
